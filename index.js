23bd79addb6080234515cc20354cc28b4ea420b4
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2024-02-20 09:49:24 +0200

    Summary

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const books = require('./books');
 
 const booksRouter = require('./routes/api/books');
+const contactsRouter = require('./routes/api/contacts')
 
 const app = express();
 
 app.use(cors());
 
+app.use('/api/contacts', contactsRouter)
 app.use('/api/books', booksRouter)
 
 app.listen(3000);
